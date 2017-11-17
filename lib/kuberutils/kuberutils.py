338d945e6e3df241aa25e931d4bf90a24d4fba77
Jason Kuruzovich <jkuruzovich@gmail.com>
2017-11-17 08:35:25 -0500

    Update configuration

--- lib/kuberutils/kuberutils.py
+++ lib/kuberutils/kuberutils.py
@@ -21,17 +21,33 @@ import sys
 import os.path
 import collections
 import csv, codecs
+import json
 from dateutil import parser
 docker_prefix="sudo docker run --rm -i --volumes-from gcloud-config kuberlytics/gcloud-sdk "
 
-def initialize(path,config_file):
+def initialize(config):
     """This functions creates a variety of commands to augment the configuration of Kubernetes.
        It returns the cf.
     """
-    with open(path+config_file, 'r') as yaml:
+    if config['restore_config']:
+        restore_kube(config)
+    #load the config file
+    with open(config['path']+config['config_file'], 'r') as yaml:
          cf=ruamel.yaml.round_trip_load(yaml, preserve_quotes=True)
-    cf['path']=path
-    cf['config_file']=config_file
+    #update passed variables
+    cf['restore_config']=False
+    cf['path']=config['path']
+    cf['config_file']=config['config_file']
+    cf['cloud_provider']=config['cloud_provider']      #azure of google
+    cf['project_name']=config['project_name']  #Must be globally unique.
+    cf['cluster_name']=config['cluster_name']         #Only use if restore_config==True
+    cf['config_file']=config['config_file']  #The configuration will be overwritten if retreived.
+    cf['path']=config['path']  #Path to admintools repository
+    cf['ssh_dir']=config['ssh_dir']     #ssh directory
+    cf['resource_group']=cf['project_name']
+    #store any local changes
+    ruamel.yaml.round_trip_dump(cf, open(config['path']+config['config_file'], 'w'))
+
     if cf['cloud_provider']=="azure":
         cf=azure_commands(cf)
     elif cf['cloud_provider']=="google":
@@ -44,7 +60,8 @@ def initialize(path,config_file):
 def gcloud_commands(cf_g):
     """This functions creates a variety of commands to augment the configuration of Kubernetes on the Google cloud platform.
     """
-    cf_g['cluster_name']=cf_g['g_cluster_name']
+    cf_g['cluster_name']=cf_g['cluster_name']
+    cf_g['g_project']=cf_g['project_name']
     cf_g['create_service_account']="gcloud iam service-accounts create "+cf_g['g_service_account_name']+ " --display-name "+ cf_g['g_service_account_name']
     cf_g['create_key']="gcloud iam service-accounts keys create "+cf_g['g_path']+"/config/gcloud/"+cf_g['g_authorization_file'] +" --iam-account "+cf_g['g_service_account_name']+"@"+cf_g['g_project']+".iam.gserviceaccount.com"
     cf_g['get_policy']="gcloud iam service-accounts get-iam-policy "+cf_g['g_service_account_name']+"@"+cf_g['g_project']+".iam.gserviceaccount.com --format json > "+cf_g['g_path']+"/config/gcloud/policy.json"
@@ -54,55 +71,55 @@ def gcloud_commands(cf_g):
     cf_g['create_project']="gcloud projects create "+cf_g['g_project']+"----set-as-default"
     cf_g['set_project']="gcloud config set project "+cf_g['g_project']
     cf_g['set_zone']="gcloud config set compute/zone "+cf_g['g_zone']
-    cf_g['create_cluster']="gcloud container clusters create "+cf_g['g_cluster_name']+" --num-nodes="+str(cf_g['g_num_nodes'])+" --machine-type="+cf_g['g_machine_type']+" --zone="+cf_g['g_zone']
-    cf_g['get_credentials']="gcloud container clusters get-credentials "+cf_g['g_cluster_name']
-    cf_g['stop_cluster']="gcloud container clusters resize "+cf_g['g_cluster_name']+" --size=0 --quiet"
-    cf_g['normal_size_cluster']="gcloud container clusters resize "+cf_g['g_cluster_name']+" --size="+str(cf_g['g_num_nodes'])+" --quiet"
-    cf_g['class_size_cluster']="gcloud container clusters resize "+cf_g['g_cluster_name']+" --size="+str(cf_g['g_num_nodes_class'])+" --quiet"
-    cf_g['delete_cluster']="gcloud container clusters delete "+cf_g['g_cluster_name']+" --zone="+cf_g['g_zone']+" --quiet"
-    cf_g['autoscale']="gcloud alpha container clusters update "+cf_g['g_cluster_name']+" --enable-autoscaling --min-nodes="+str(cf_g['g_num_nodes'])+" --max-nodes="+str(cf_g['g_max_nodes'])+" --zone="+cf_g['g_zone']+" --node-pool=default-pool"
+    cf_g['create_cluster']="gcloud container clusters create "+cf_g['cluster_name']+" --num-nodes="+str(cf_g['g_num_nodes'])+" --machine-type="+cf_g['g_machine_type']+" --zone="+cf_g['g_zone']
+    cf_g['get_credentials']="gcloud container clusters get-credentials "+cf_g['cluster_name']
+    cf_g['stop_cluster']="gcloud container clusters resize "+cf_g['cluster_name']+" --size=0 --quiet"
+    cf_g['normal_size_cluster']="gcloud container clusters resize "+cf_g['cluster_name']+" --size="+str(cf_g['g_num_nodes'])+" --quiet"
+    cf_g['class_size_cluster']="gcloud container clusters resize "+cf_g['cluster_name']+" --size="+str(cf_g['g_num_nodes_class'])+" --quiet"
+    cf_g['delete_cluster']="gcloud container clusters delete "+cf_g['cluster_name']+" --zone="+cf_g['g_zone']+" --quiet"
+    cf_g['autoscale']="gcloud alpha container clusters update "+cf_g['cluster_name']+" --enable-autoscaling --min-nodes="+str(cf_g['g_num_nodes'])+" --max-nodes="+str(cf_g['g_max_nodes'])+" --zone="+cf_g['g_zone']+" --node-pool=default-pool"
     cf_g['create_fixedip']="gcloud compute addresses create "+cf_g['g_fixedip_namespace']+" --region="+cf_g['g_region']
     cf_g['describe_fixedip']="gcloud compute addresses describe "+cf_g['g_fixedip_namespace']+" --region="+cf_g['g_region']
     cf_g['delete_forwarding_rule']="gcloud compute forwarding-rules delete forwarding_rule --quiet"
     cf_g['delete_fixedip']="gcloud compute addresses delete "+cf_g['g_fixedip_namespace']+" --region="+cf_g['g_region']+" --quiet"
-    cf_g['describe_cluster']="gcloud container clusters describe "+cf_g['g_cluster_name']
+    cf_g['describe_cluster']="gcloud container clusters describe "+cf_g['cluster_name']
     return cf_g
 
 def azure_commands(cf_a):
     """This functions creates a variety of commands to augment the configuration of Kubernetes on the Google cloud platform.
     """
-    cf_a['cluster_name']=cf_a['a_cluster_name']
+
     cf_a['web_login']="az login"
     cf_a['login']="echo 'Service account login not yet available for Azure'"
-    cf_a['create_project']="az group create --name="+cf_a['a_resource_group']+ " --location="+ cf_a['a_location']
+    cf_a['create_project']="az group create --name="+cf_a['project_name']+ " --location="+ cf_a['a_location']
     cf_a['set_project']="echo 'No need to set project in Azure.'"
     cf_a['set_zone']="echo 'No need to set project in Azure.'"
-    cf_a['delete_project']="az group delete --name="+cf_a['a_resource_group']+" --yes --no-wait"
+    cf_a['delete_project']="az group delete --name="+cf_a['project_name']+" --yes --no-wait"
     cf_a['list-locations']="az account list-locations"
     cf_a['autoscale']="echo 'Autoscaling currently not possible.'"
     if cf_a['a_service_type']=="aks":
-        cf_a['create_cluster']="az aks create --resource-group="+cf_a['a_resource_group'] +" --name="+cf_a['a_cluster_name']+" --agent-count="+str(cf_a['a_num_nodes'])+" --agent-vm-size="+cf_a['a_machine_type']+" --generate-ssh-keys --no-wait"
-        cf_a['describe_cluster']="az aks list --resource-group="+cf_a['a_resource_group']
-        cf_a['delete_cluster']="az aks delete --name="+cf_a['a_cluster_name']+" --resource-group="+cf_a['a_resource_group']+" --no-wait"
-        cf_a['get_credentials']="az aks get-credentials --resource-group="+cf_a['a_resource_group']+" --name="+cf_a['a_cluster_name']
-        cf_a['normal_size_cluster']="az aks scale  --agent-count "+str(cf_a['a_num_nodes']) +" --name="+cf_a['a_cluster_name']+" --resource-group="+cf_a['a_resource_group']
-        cf_a['class_size_cluster']="az aks scale --agent-count "+ str(cf_a['a_num_nodes_class'])+" --name="+cf_a['a_cluster_name']+" --resource-group="+cf_a['a_resource_group']
+        cf_a['create_cluster']="az aks create --resource-group="+cf_a['project_name'] +" --name="+cf_a['cluster_name']+" --agent-count="+str(cf_a['a_num_nodes'])+" --agent-vm-size="+cf_a['a_machine_type']+" --generate-ssh-keys --no-wait"
+        cf_a['describe_cluster']="az aks list --resource-group="+cf_a['project_name']
+        cf_a['delete_cluster']="az aks delete --name="+cf_a['cluster_name']+" --resource-group="+cf_a['project_name']+" --no-wait"
+        cf_a['get_credentials']="az aks get-credentials --resource-group="+cf_a['project_name']+" --name="+cf_a['cluster_name']
+        cf_a['normal_size_cluster']="az aks scale  --agent-count "+str(cf_a['a_num_nodes']) +" --name="+cf_a['cluster_name']+" --resource-group="+cf_a['project_name']
+        cf_a['class_size_cluster']="az aks scale --agent-count "+ str(cf_a['a_num_nodes_class'])+" --name="+cf_a['cluster_name']+" --resource-group="+cf_a['project_name']
     elif cf_a['a_service_type']=="acs":
-        cf_a['create_cluster']="az acs create --orchestrator-type=kubernetes --resource-group="+cf_a['a_resource_group'] +" --name="+cf_a['a_cluster_name']+" --dns-prefix="+cf_a['a_dns_prefix']+" --agent-count="+str(cf_a['a_num_nodes'])+" --agent-vm-size="+cf_a['a_machine_type']+" --generate-ssh-keys --no-wait"
-        cf_a['describe_cluster']="az acs list  --resource-group="+cf_a['a_resource_group']
-        cf_a['delete_cluster']="az acs delete  --resource-group="+cf_a['a_resource_group']+" --name="+cf_a['a_cluster_name']+" --no-wait"
-        cf_a['get_credentials']="az acs kubernetes get-credentials --resource-group="+cf_a['a_resource_group']+" --name="+cf_a['a_cluster_name']+" --ssh-key-file=~/.ssh/id_rsa_"+cf_a['a_cluster_name']
-        cf_a['normal_size_cluster']="az acs scale --resource-group="+cf_a['a_resource_group']+" --name="+cf_a['a_cluster_name']+" --new-agent-count "+str(cf_a['a_num_nodes'])
-        cf_a['class_size_cluster']="az acs scale --resource-group="+cf_a['a_resource_group']+" --name="+cf_a['a_cluster_name']+" --new-agent-count "+str(cf_a['a_num_nodes_class'])
+        cf_a['create_cluster']="az acs create --orchestrator-type=kubernetes --resource-group="+cf_a['project_name'] +" --name="+cf_a['cluster_name']+" --dns-prefix="+cf_a['a_dns_prefix']+" --agent-count="+str(cf_a['a_num_nodes'])+" --agent-vm-size="+cf_a['a_machine_type']+" --generate-ssh-keys --no-wait"
+        cf_a['describe_cluster']="az acs list  --resource-group="+cf_a['project_name']
+        cf_a['delete_cluster']="az acs delete  --resource-group="+cf_a['project_name']+" --name="+cf_a['cluster_name']+" --no-wait"
+        cf_a['get_credentials']="az acs kubernetes get-credentials --resource-group="+cf_a['project_name']+" --name="+cf_a['cluster_name']+" --ssh-key-file=~/.ssh/id_rsa_"+cf_a['cluster_name']
+        cf_a['normal_size_cluster']="az acs scale --resource-group="+cf_a['project_name']+" --name="+cf_a['cluster_name']+" --new-agent-count "+str(cf_a['a_num_nodes'])
+        cf_a['class_size_cluster']="az acs scale --resource-group="+cf_a['project_name']+" --name="+cf_a['cluster_name']+" --new-agent-count "+str(cf_a['a_num_nodes_class'])
     cf_a['install_helm']='helm init --client-only'
-    # Can't size to 0 cf_g['a_stop_cluster']="az acs scale --resource-group="+cf_a['a_resource_group']+" --name="+cf_a['a_cluster_name']+" --new-agent-count 0"
-    cf_a['create_storage']= "az storage account create --resource-group="+cf_a['a_resource_group']+" --location="+cf_a['a_location']+" --sku=Standard_LRS  --name="+cf_a['a_storage_account']+" --kind=Storage"
-    cf_a['get_storage_key']="az storage account keys list --account-name="+cf_a['a_storage_account']+" --resource-group="+cf_a['a_resource_group']+" --output=json | jq .[0].value -r"
-    cf_a['create_keyvault']="az keyvault create --name="+cf_a['a_cluster_name']+" --resource-group="+ cf_a['a_resource_group']+" --location="+cf_a['a_location']+" --enabled-for-template-deployment true"
-    cf_a['backup_private_key']="az keyvault secret set --vault-name="+ cf_a['a_cluster_name']+ " --name=id-rsa-"+cf_a['a_cluster_name']+" --file=~/.ssh/id_rsa_"+cf_a['a_cluster_name']
-    cf_a['backup_public_key']="az keyvault secret set --vault-name="+ cf_a['a_cluster_name']+ " --name=id-rsa"+cf_a['a_cluster_name']+"-pub --file=~/.ssh/id_rsa_"+cf_a['a_cluster_name']+".pub"
-    cf_a['get_private_key']="az keyvault secret show --vault-name="+ cf_a['a_cluster_name']+ " --name=id-rsa-"+cf_a['a_cluster_name']
-    cf_a['get_public_key']="az keyvault secret show --vault-name="+ cf_a['a_cluster_name']+ " --name=id-rsa"+cf_a['a_cluster_name']+"-pub"
+    # Can't size to 0 cf_g['a_stop_cluster']="az acs scale --resource-group="+cf_a['project_name']+" --name="+cf_a['cluster_name']+" --new-agent-count 0"
+    cf_a['create_storage']= "az storage account create --resource-group="+cf_a['project_name']+" --location="+cf_a['a_location']+" --sku=Standard_LRS  --name="+cf_a['a_storage_account']+" --kind=Storage"
+    cf_a['get_storage_key']="az storage account keys list --account-name="+cf_a['a_storage_account']+" --resource-group="+cf_a['project_name']+" --output=json | jq .[0].value -r"
+    cf_a['create_keyvault']="az keyvault create --name="+cf_a['cluster_name']+" --resource-group="+ cf_a['project_name']+" --location="+cf_a['a_location']+" --enabled-for-template-deployment true"
+    cf_a['backup_private_key']="az keyvault secret set --vault-name="+ cf_a['cluster_name']+ " --name=id-rsa-"+cf_a['cluster_name']+" --file=~/.ssh/id_rsa_"+cf_a['cluster_name']
+    cf_a['backup_public_key']="az keyvault secret set --vault-name="+ cf_a['cluster_name']+ " --name=id-rsa"+cf_a['cluster_name']+"-pub --file=~/.ssh/id_rsa_"+cf_a['cluster_name']+".pub"
+    cf_a['get_private_key']="az keyvault secret show --vault-name="+ cf_a['cluster_name']+ " --name=id-rsa-"+cf_a['cluster_name']
+    cf_a['get_public_key']="az keyvault secret show --vault-name="+ cf_a['cluster_name']+ " --name=id-rsa"+cf_a['cluster_name']+"-pub"
     return cf_a
 
 def jupyterhub_commands(cf_j):
@@ -124,6 +141,33 @@ def jupyterhub_commands(cf_j):
     cf_j['jup_callback_url']=cf_j['jup_callback_url']+cf_j['jup_url']+"/hub/oauth_callback"
     cf_j['jup_get_logs']="kubectl --namespace="+cf_j['jup_namespace']+" logs <insert_podname>"
     return cf_j
+
+def restore_kube(cf):
+    restore_file_az(cf['cluster_name']+"-public-key", cf['ssh_dir']+"id_rsa_"+cf['cluster_name']+".pub", cf)
+    restore_file_az(cf['cluster_name']+"-private-key", cf['ssh_dir']+"id_rsa_"+cf['cluster_name'], cf)
+    restore_file_az(cf['cluster_name']+"-config-file", cf['path']+cf['config_file'], cf)
+    return
+
+def restore_file_az(secret, file, cf):
+    result=bash_command("az keyvault secret show --vault-name="+ cf['cluster_name']+ " --name="+secret)
+    result=json.loads(result)
+    with open(file, "w+") as text_file:
+          text_file.write(result['value'])
+    return result
+
+def backup_kube(cf):
+    bash_command('create_keyvault', cf)
+    if cf['cloud_provider']=='azure':
+        backup_file_az(cf['cluster_name']+"-public-key", cf['ssh_dir']+"id_rsa_"+cf['cluster_name']+".pub", cf)
+        backup_file_az(cf['cluster_name']+"-private-key", cf['ssh_dir']+"id_rsa_"+cf['cluster_name'], cf)
+        backup_file_az(cf['cluster_name']+"-config-file", cf['path']+cf['config_file'], cf)
+    else:
+        print('Backup not available for your cloud provider.')
+    return
+
+def backup_file_az(secret, file, cf):
+    result=bash_command("az keyvault secret set --vault-name="+ cf['cluster_name']+ " --name="+secret+" --file="+file)
+    return result
 
 def import_config(file):
     kube_yaml='../../config/kubernetes.yaml'
@@ -199,9 +243,11 @@ def set_jupyterhub_config(cf):
         if cf['cloud_provider']=='azure':
             config['prePuller']=cf['jup_prePuller']
         ruamel.yaml.round_trip_dump(config, open(cf['jup_config'], 'w'))
+        cf['jup_rebuild_config']=False
+        ruamel.yaml.round_trip_dump(cf, open(cf['path']+cf['config_file'], 'w'))
     else:
         print("Currently the jup_rebuild_config parameter in the configuration file is set to false. Not rebuilding config.")
-    return
+    return cf
 
 def bash_command2(command):
     import subprocess
